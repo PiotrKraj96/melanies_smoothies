1a8f4267373e3e4f503abe00ffdc77bf0d80297f
PiotrK <44431293+PiotrKraj96@users.noreply.github.com>
2025-05-13 13:35:44 +0200

    add api call to streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
+
 # Write directly to the app
 st.title(
     ":cup_with_straw: Customize Your Smoothie! :cup_with_straw:"
@@ -43,7 +45,8 @@ if ingredients_list:
     # st.write(my_insert_stmt)
 
     time_to_insert = st.button("Submit Order")
-
+    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+    st.text(smoothiefroot_response)
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
 
